4dc37dd70ee3ae2d39ae2b2e348659e8bdfe9699
Cosmin Poieana <cmin764@gmail.com>
2024-10-15 18:04:21 +0300

    Split concerns correctly: database vs. deps

--- deep_ice/core/database.py
+++ deep_ice/core/database.py
@@ -1,5 +1,18 @@
-from sqlmodel import create_engine
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from deep_ice.core.config import settings
 
-engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI), echo=True, future=True)
+async_engine = create_async_engine(
+    str(settings.SQLALCHEMY_DATABASE_URI), echo=True, future=True
+)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async_session = async_sessionmaker(
+        bind=async_engine, class_=AsyncSession, expire_on_commit=False
+    )
+    async with async_session() as session:
+        yield session
